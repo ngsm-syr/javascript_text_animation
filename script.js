94397aceb0c864bebe0595cfd050eeef750e354d
Nagashima Sayuri <106061832+ngsm-syr@users.noreply.github.com>
2023-10-20 13:29:16 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+// JavaScript
+
+// フェード
+const fade = document.querySelector('.fade');
+
+const fadeKeyframes = {
+  opacity: [0, 1],
+}
+
+const fadeOptions = {
+  duration: 3000,
+  easing: 'ease',
+  fill: 'forwards',
+}
+
+fade.animate(fadeKeyframes, fadeOptions);
+
+// 回転
+const spin = document.querySelector('.spin');
+
+const spinKeyframes = {
+  rotate: ['x 360deg','x 0deg'],
+  opacity: [0, 1],
+}
+
+const spinOptions = {
+  duration: 2000,
+  easing: 'ease',
+}
+
+spin.animate(spinKeyframes, spinOptions);
+
+// 色が変わる
+const color = document.querySelector('.color');
+
+const colorKeyframes = {
+  backgroundColor: ['orange ','yellow ','lightgreen','slyblue'],
+}
+
+const colorOptions = {
+  duration: 5000,
+  direction: 'alternate',
+  iterations: Infinity,
+}
+
+color.animate(colorKeyframes,colorOptions);
+
+
+// 背景が延びる
+const line = document.querySelector('.line');
+
+const lineKeyframes = {
+  width: ['0','100%'],
+  color: ['transparent','#FFF'],
+}
+
+const lineOptions = {
+  duration: 2000,
+  fill: 'forwards',
+}
+
+line.animate(lineKeyframes,lineOptions);
+
+
+// 文字のスライドイン
+const slide = document.querySelectorAll('.slide-in');
+for (let i = 0; i < slide.length; i++) {
+
+  const keyframes = {
+    opacity: [0, 1],
+    translate: ['0 50px', 0],
+  };
+  
+  const options = {
+    duration: 600,
+    delay: i * 400,
+    fill: 'forwards',
+  };
+
+  slide[i].animate(keyframes, options);
+
+}
